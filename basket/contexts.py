bba20f813e2e2d73de9aa135155ff9493814bdfb
siondawson <siondawson91@gmail.com>
2023-11-11 18:02:03 +0000

    add basket context

---
+++ basket/contexts.py
@@ -0,0 +1,19 @@
+from decimal import Decimal
+from django.conf import settings 
+
+def basket_contents(request):  # context processor to make bag contents available ot whole application
+
+    basket_items = []
+    total = 0
+    basket_count = 0
+
+    context = {
+        'basket_items': basket_items,
+        'total': total,
+        'basket_count': basket_count,
+        'grand_total': grand_total,
+    }
+
+    grand_total = total
+
+    return context
