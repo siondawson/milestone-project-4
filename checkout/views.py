ac288c034fea9bcd29e24558d1a1cdcf406e9f48
siondawson <siondawson91@gmail.com>
2023-11-16 16:53:41 +0000

    working on payment confirmation

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from sheet_music.models import Sheetmusic
 from basket.contexts import basket_contents
 
 import stripe
@@ -13,23 +15,54 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
+    if request.method == 'POST':
 
-    basket = request.session.get('basket', {})
-    if not basket:
-        messages.error(request, "There's nothing in your basket at the moment")
-        return redirect(reverse('sheetmusic'))
+        basket = request.session.get('basket', {})
 
-    current_basket = basket_contents(request)
-    total = current_basket['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+
+            for sheetmusic_id, sheetmusic_data in basket.items():
+                try:
+                    sheetmusic = Sheetmusic.objects.get(id=sheetmusic_id)
+                    if isinstance(sheetmusic_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            sheetmusic=sheetmusic,
+                            quantity=quantity
+                        )
+                    order_line_item.save()
+                except Sheetmusic.DoesNotExist:
+                    messages.error(request, "One of the titles in your basket wasn't found in our database! Please get in touch for help!")
+                    order.delete()
+                    return redirect(reverse('view_basket'))
+            return redirect(reverse('checkout-success', args=[order.order_number]))
+
+        else:
+            messages.error(request, 'There was an error with your form. Please double check your information')
+    else:
+
+        basket = request.session.get('basket', {})
+        if not basket:
+            messages.error(request, "There's nothing in your basket at the moment")
+            return redirect(reverse('sheetmusic'))
+
+        current_basket = basket_contents(request)
+        total = current_basket['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
 
 
-    order_form = OrderForm()
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your environment?')
@@ -41,6 +74,27 @@ def checkout(request):
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handles successful checkouts.
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! Your order number is {order.number}.  \
+                                A confirmation email will be sent to {order.email}. \
+                                Your sheet music is available to download right away')
+
+    if 'basket' in request.session:
+        del request.session['basket']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
     }
 
     return render(request, template, context)
