dc08077ef7917541ed23ce039cf87a6b222fd840
siondawson <siondawson91@gmail.com>
2023-11-17 20:17:25 +0000

    signals.py not working. attributeerror on update_total

--- checkout/signals.py
+++ checkout/signals.py
@@ -13,9 +13,9 @@ def update_on_save(sender, instance, created, **kwargs):
 
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
     """
     update grand total on lineitem delete
     """
-
+    print('delete signal received')
     instance.order.update_total()
