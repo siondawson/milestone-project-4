f1070a61766d8ee889958653c59563e32fe6e459
siondawson <siondawson91@gmail.com>
2023-11-18 17:24:36 +0000

    payment system complete. webhook error on basket

--- checkout/views.py
+++ checkout/views.py
@@ -49,8 +49,12 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
-            print(order)
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_basket = json.dumps(basket)
+            order.save()
+            
 
             for sheetmusic_id, sheetmusic_data in basket.items():
                 try:
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 
+from .models import Order, OrderLineItem
+from sheet_music.models import Sheetmusic
+
+import json
+import time
+
 
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
@@ -20,11 +26,84 @@ class StripeWH_Handler:
         Handle the payment_intent.succeeded webhook from Stripe
         """
         intent = event.data.object
-        print(intent)
-        return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
-            status=200)
+        pid = intent.id
+        basket = intent.metadata.basket
+        save_info = intent.metadata.save_info
 
+        # Get the Charge object
+        stripe_charge = stripe.Charge.retrieve(
+            intent.latest_charge
+        )
+
+        billing_details = stripe_charge.billing_details # updated
+        grand_total = round(intent.charges.data[0].amount / 100, 2) # updated
+
+        # clean data in the shipping details
+        for field, value in shipping_detials.address.items():
+            if value == "":
+                shipping_detials.address[field] = None
+
+        order_exists = False
+        attempt = 1
+        while attempt <= 5:
+            try:
+                order = Order.objects.get(
+                    full_name__iexact=shipping_details.name,
+                    email__iexact=billing_details.email,
+                    phone_number__iexact=shipping_details.phone,
+                    country__iexact=shipping_details.address.country,
+                    postcode__iexact=shipping_details.address.postal_code,
+                    town_or_city__iexact=shipping_details.address.city,
+                    street_address1__iexact=shipping_details.address.line1,
+                    street_address2__iexact=shipping_details.address.line2,
+                    county__iexact=shipping_details.address.state,
+                    grand_total=grand_total,
+                    original_basket=basket,
+                )
+                order_exists = True
+                break
+
+                
+            except Order.DoesNotExist:
+                attempt += 1
+                time.sleep(1)
+        if order_exists:
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]}',
+                status=200)
+        else:
+            order = None
+            try:
+                order = Order.objects.create(
+                    full_name=shipping_details.name,
+                    email=billing_details.email,
+                    phone_numbet=shipping_details.phone,
+                    country=shipping_details.address.country,
+                    postcode=shipping_details.address.postal_code,
+                    town_or_city=shipping_details.address.city,
+                    street_address1=shipping_details.address.line1,
+                    street_address2=shipping_details.address.line2,
+                    county=shipping_details.address.state,
+                    grand_total=grand_total,
+                    original_basket=basket,
+                )
+                for sheetmusic_id, sheetmusic_data in json.loads(basket).items():
+                    sheetmusic = Sheetmusic.objects.get(id=sheetmusic_id)
+                    if isinstance(sheetmusic_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            sheetmusic=sheetmusic,
+                            quantity=sheetmusic_data,
+                        )
+                    order_line_item.save()
+            except Exception as e:
+                if order:
+                    order.delete()
+                return HttpResponse(content=f'Webhook received: {event["type"]} | ERROR: {e}', 
+                status=500)
+        return HttpResponse(
+                content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+                status=200)
     def handle_payment_intent_payment_failed(self, event):
         """
         Handle the payment_intent.payment_failed webhook from Stripe
