f544b69978d66287000c2bc94bd169b80e1b112d
siondawson <siondawson91@gmail.com>
2023-11-27 14:30:18 +0000

    add forms.py to sheet_music

---
+++ sheet_music/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Sheetmusic, Category
+
+class SheetmusicForm(forms.ModelForm):
+
+    class Meta:
+        model = Sheetmusic 
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
