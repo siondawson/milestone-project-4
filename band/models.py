d1e483fdbfcbb8ebe87f706f90c9d7365b92b2c2
siondawson <siondawson91@gmail.com>
2023-11-28 15:05:39 +0000

    add sendemail app, templates, urls and views

--- band/models.py
+++ band/models.py
@@ -9,3 +9,9 @@ class Concert(models.Model):
     city = models.CharField(max_length=50)
     tickets = models.CharField(max_length=300, null=False)
 
+
+class Enquiry(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    message = models.TextField(null=False, blank=False)
