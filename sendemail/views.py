02e5be4c86e8e5d389b6d508112a771a488618a8
siondawson <siondawson91@gmail.com>
2023-11-29 12:54:06 +0000

    added email_sent.html to sendemail success view

--- sendemail/views.py
+++ sendemail/views.py
@@ -2,6 +2,7 @@ from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .forms import ContactForm
+from django.contrib import messages
 
 
 # email app tutorial from https://learndjango.com/tutorials/django-email-contact-form-tutorial
@@ -14,13 +15,23 @@ def contact(request):
         if form.is_valid():
             subject = form.cleaned_data["subject"]
             from_email = form.cleaned_data["from_email"]
+            phone_number = form.cleaned_data["phone_number"]
             message = form.cleaned_data['message']
             try:
-                send_mail(subject, message, from_email, ["admin@example.com"])
+                send_mail(subject, message, from_email, phone_number, ["admin@example.com"])
             except BadHeaderError:
                 return HttpResponse("Invalid header found.")
+            messages.success(request, "Email sent! We will respond as soon as possible")
             return redirect("success")
-    return render(request, "email.html", {"form": form})
+    return render(request, "sendemail/email.html", {"form": form})
+
 
 def success(request):
-    return HttpResponse("Success! Thank you for your message.")
+
+
+    template = "sendemail/email_sent.html"
+    context = {
+
+    }
+
+    return render(request, template, context)
