4facb23c9cebb10b51cb711f0906b1b1af1353ba
siondawson <siondawson91@gmail.com>
2023-12-05 15:45:14 +0000

    updated base_dir in settings.py

--- absolute_zero/settings.py
+++ absolute_zero/settings.py
@@ -14,12 +14,14 @@ import os
 from pathlib import Path
 import dj_database_url
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 if os.path.exists('env.py'):
     import env
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
