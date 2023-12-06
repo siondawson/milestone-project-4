c25290dd9fe71cb72c353558324bf7a74604edbb
siondawson <siondawson91@gmail.com>
2023-12-06 15:40:08 +0000

    reverted settings back

--- absolute_zero/settings.py
+++ absolute_zero/settings.py
@@ -186,12 +186,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
- # m
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
-if 'DEVELOPMENT' in os.environ: 
-    STATIC_URL = '/static/'
-else: STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),) 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
