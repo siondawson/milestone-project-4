4a4e3b4031db8ca001c70d71de3ef1829dc0807e
siondawson <siondawson91@gmail.com>
2023-12-10 13:16:20 +0000

    fixed pdf download functionality

---
+++ upload.js
@@ -0,0 +1,8 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dq8vv2jda', 
+  api_key: '587244914819126', 
+  api_secret: '***************************' 
+});
+
