df5605ff67e89420cf273eb3267e1f247658c600
siondawson <siondawson91@gmail.com>
2023-12-12 12:51:28 +0000

    upload wireframes and minor font class additions

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<str:order_number>/', views.order_history,
          name='order_history'),
-    path('order_history/<str:order_number>/pdf/<str:pdf_file>',
-         views.download_sheetmusic, name='download_sheetmusic'),
 ]
